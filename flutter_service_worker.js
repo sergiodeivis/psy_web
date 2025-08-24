b273364d6463944fe9d410e1f684b4aa6bccf8f6
Sérgio Deivis <gtisergio@gmail.com>
2025-08-24 16:19:05 -0300

    23

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "af64b866cbbdfbc30181a701fc867c89
 "assets/AssetManifest.json": "5224db365d56cd5267a1eaf77a9d77b6",
 "assets/assets/images/Icon.png": "a981b915794dfbd8df37f1789e854f39",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "f552e00b05ad6099a6b64e22cacfa58a",
+"assets/fonts/MaterialIcons-Regular.otf": "c32d1d498e0cf5e79947e02db421a437",
 "assets/NOTICES": "b1c5bee2817283bdf83c0486c76758d1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "af64b866cbbdfbc30181a701fc867c89
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "6e4ed13efe32a2bcf91b3c9ef40c2234",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5281477a8753199beba50021d0e8c7af",
+"flutter_bootstrap.js": "1ce6fccb2f5779bddcb66cc96280b4a5",
 "icons/Icon-192.png": "281a12b5353f2b16c96f3f61b8a2b544",
 "icons/Icon-512.png": "a981b915794dfbd8df37f1789e854f39",
 "icons/Icon-maskable-192.png": "3de0817fa622a515f3e5c2433a4bb79a",
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "af64b866cbbdfbc30181a701fc867c89
 "/": "d9921b4d96830661c513f984956c3a7b",
 "index.html.bak": "d9921b4d96830661c513f984956c3a7b",
 "install_prompt.js": "cee960a38f4f4d875d055f4de06efe29",
-"main.dart.js": "e66026c2cd7fab499fd3d7674cc14b7d",
+"main.dart.js": "b564cec623e2b969001c4c522e014a8b",
 "manifest.json": "d6a157f3065d00d0201decd63c28a5ee",
 "version.json": "796b71f6ccef12607358a6ef0ee4f5dc"};
 // The application shell files that are downloaded before a service worker can
